946b57d8465161e59198c2c0cb03da8b43dda005
Jimmy Fang <fangjian0423@gmail.com>
2022-12-21 11:18:31 +0000

    push

--- infra/modules/containerapps/containerapps_output.tf
+++ infra/modules/containerapps/containerapps_output.tf
@@ -6,6 +6,10 @@ output "CONTAINER_APP_URI" {
   value = "https://${jsondecode(azapi_resource.container_apps.output).properties.configuration.ingress.fqdn}"
 }
 
+output "CONTAINER_APP_ID" {
+  value = azapi_resource.container_apps.id
+}
+
 output "CONTAINER_APP_NAME_IDENTITY_PRINCIPAL_ID" {
   value     = length(azapi_resource.container_apps.identity) == 0 ? "" : azapi_resource.container_apps.identity.0.principal_id
   sensitive = true
